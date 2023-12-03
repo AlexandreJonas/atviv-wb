0d09e3ebf4b90f0e76448500c624b64d526a75fe
anxjos <jonassouzafonseca@gmail.com>
2023-12-03 20:29:53 -0300

    idk 2

--- front/src/componentes/formularioCadastroCliente.tsx
+++ front/src/componentes/formularioCadastroCliente.tsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { useEffect, useState } from "react";
 import Cliente from "../model/cliente";
+import Endereco from "../model/endereco";
 
 type props = {
     tema: string
@@ -11,6 +12,7 @@ type props = {
 export default function FormularioCadastroCliente(props: props) {
 
     const [cliente, setCliente] = useState(new Cliente('', ''))
+    const [endereco, setEndereco] = useState(new Endereco('',''))
 
     const getCliente = async () => {
         if (props.id !== 0) {
@@ -18,6 +20,7 @@ export default function FormularioCadastroCliente(props: props) {
                 const response = await fetch(`http://localhost:32832/cliente/${props.id}`);
                 const jsonData = await response.json();
                 setCliente(jsonData)
+                setEndereco(jsonData.endereco)
             } catch (error: any) {
                 console.log(error.message)
             }
@@ -36,32 +39,42 @@ export default function FormularioCadastroCliente(props: props) {
         }));
     };
 
-    const deletar = async(e:any) => {
+    const handleChangeAddr = (e: { target: { name: any; value: any; }; }) => {
+        const { name, value } = e.target;
+        setEndereco(prevEndereco => ({
+            ...prevEndereco,
+            [name]: value
+        }));
+    };
+
+    const deletar = async (e: any) => {
         try {
             const response = await fetch(`http://localhost:32832/cliente/excluir`, {
                 method: "DELETE",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(cliente),
-              });
-        } catch (error:any) {
+            });
+        } catch (error: any) {
             console.log(error.message)
         }
-        props.seletorView(0,'Clientes', e) 
+        props.seletorView(0, 'Clientes', e)
     }
 
     const inserir = async () => {
         try {
+            let AuxCliente = cliente
+            AuxCliente.endereco = endereco
             const response = await fetch(`http://localhost:32832/cliente/cadastrar`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(cliente),
+                body: JSON.stringify(AuxCliente),
             });
             if (response.ok) {
                 console.log("Cliente criado com sucesso!");
             } else {
                 throw new Error("Erro ao inserir cliente");
             }
-            
+
         } catch (error: any) {
             console.log(error.message)
         }
@@ -69,10 +82,12 @@ export default function FormularioCadastroCliente(props: props) {
 
     const atualizar = async () => {
         try {
+            let AuxCliente = cliente
+            AuxCliente.endereco = endereco
             const response = await fetch(`http://localhost:32832/cliente/atualizar`, {
                 method: "PUT",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(cliente),
+                body: JSON.stringify(AuxCliente),
             });
             if (response.ok) {
                 console.log("Cliente criado com sucesso!");
@@ -84,14 +99,14 @@ export default function FormularioCadastroCliente(props: props) {
         }
     }
 
-    const salvar = (e:any) => {
+    const salvar = (e: any) => {
         if (props.id !== 0) {
             atualizar()
         }
         else {
             inserir()
         }
-       props.seletorView(0,'Clientes', e)   
+        props.seletorView(0, 'Clientes', e)
     }
 
     let estiloBotao = `btn waves-effect waves-light col left ${props.tema}`
@@ -100,66 +115,68 @@ export default function FormularioCadastroCliente(props: props) {
             <form className="col s12">
                 <div className="row">
                     <div className="input-field col s6">
-                        <input defaultValue={cliente.nome} onChange={handleChange} id="first_name" type="text" className="validate" name='nome' />
-                        <label htmlFor="first_name">Nome</label>
+                        <input  defaultValue={cliente.nome} onChange={handleChange} id="first_name" type="text" className="validate" name='nome' />
+                        <label className="active" htmlFor="first_name">Nome</label>
                     </div>
                     <div className="input-field col s6">
                         <input defaultValue={cliente.sobreNome} onChange={handleChange} id="last_name" type="text" className="validate" name='sobreNome' />
-                        <label htmlFor="last_name">Sobrenome</label>
+                        <label className="active" htmlFor="last_name">Sobrenome</label>
                     </div>
                 </div>
-                {/* <div className="row">
+                <div className="row">
                     <div className="input-field col s6">
-                        <input defaultValue={cliente.endereco.estado} onChange={handleChange} id="endereco_estado" type="text" className="validate" name='estado' />
-                        <label htmlFor="endereco_estado">estado</label>
+                        <input defaultValue={endereco.estado} onChange={handleChangeAddr} id="endereco_estado" type="text" className="validate" name='estado' />
+                        <label className="active" htmlFor="endereco_estado">Estado</label>
                     </div>
                     <div className="input-field col s6">
-                        <input defaultValue={cliente.endereco.cidade} onChange={handleChange} id="endereco_cidade" type="text" className="validate" name='cidade' />
-                        <label htmlFor="endereco_cidade">cidade</label>
+                        <input defaultValue={endereco.cidade} onChange={handleChangeAddr} id="endereco_cidade" type="text" className="validate" name='cidade' />
+                        <label className="active" htmlFor="endereco_cidade">Cidade</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field col s6">
+                        <input defaultValue={endereco.bairro} onChange={handleChangeAddr} id="endereco_bairro" type="text" className="validate" name='bairro' />
+                        <label className="active" htmlFor="endereco_bairro">Bairro</label>
                     </div>
                     <div className="input-field col s6">
-                        <input defaultValue={cliente.endereco.bairro} onChange={handleChange} id="endereco_bairro" type="text" className="validate" name='bairro' />
-                        <label htmlFor="endereco_bairro">bairro</label>
+                        <input defaultValue={endereco.rua} onChange={handleChangeAddr} id="endereco.rua" type="text" className="validate" name='rua' />
+                        <label className="active" htmlFor="endereco_rua">Rua</label>
                     </div>
                     <div className="input-field col s6">
-                        <input defaultValue={cliente.endereco.rua} onChange={handleChange} id="endereco_rua" type="text" className="validate" name='rua' />
-                        <label htmlFor="endereco_rua">rua</label>
+                        <input defaultValue={endereco.numero} onChange={handleChangeAddr} id="endereco_numero" type="text" className="validate" name='numero' />
+                        <label className="active" htmlFor="endereco_numero">Número</label>
                     </div>
                     <div className="input-field col s6">
-                        <input defaultValue={cliente.endereco.numero} onChange={handleChange} id="endereco_numero" type="text" className="validate" name='numero' />
-                        <label htmlFor="endereco_numero">numero</label>
+                        <input defaultValue={endereco.codigoPostal} onChange={handleChangeAddr} id="endereco_codigo_postal" type="text" className="validate" name='codigoPostal' />
+                        <label className="active" htmlFor="endereco_codigo_postal">Código Postal</label>
                     </div>
                     <div className="input-field col s6">
-                        <input defaultValue={cliente.endereco.codigoPostal} onChange={handleChange} id="endereco_codigo_postal" type="text" className="validate" name='codigoPostal' />
-                        <label htmlFor="endereco_codigo_postal">codigo postal</label>
+                        <input defaultValue={endereco.informacoesAdicionais} onChange={handleChangeAddr} id="endereco_info" type="text" className="validate" name='informacoesAdicionais' />
+                        <label className="active" htmlFor="endereco_info">Informações Adicionais</label>
                     </div>
-                    <div className="input-field col s6">
-                        <input defaultValue={cliente.endereco.informacoesAdicionais} onChange={handleChange} id="endereco_info" type="text" className="validate" name='InfoAdicional' />
-                        <label htmlFor="endereco_info">informacoes adicionais</label>
-                    </div>
-                </div>  */}
+                </div>
 
                 <div className="row">
                     <div className="input-field col s6">
                         <input id="telefone" type="text" className="validate" />
-                        <label htmlFor="telefone">Telefone</label>
+                        <label className="active" htmlFor="telefone">Telefone</label>
                     </div>
                     <div className="input-field col s6">
                         <input defaultValue={cliente.email} onChange={handleChange} id="email" type="email" className="validate" name='email' />
-                        <label htmlFor="email">E-mail</label>
+                        <label className="active" htmlFor="email">E-mail</label>
                     </div>
                 </div>
 
             </form>
             <div className="">
                 <div className="row">
-                    <button className={estiloBotao} onClick={ (e) => salvar(e)}>Salvar
+                    <button className={estiloBotao} onClick={(e) => salvar(e)}>Salvar
                         <i className="material-icons right">send</i>
                     </button>
 
                     <a className="col s1 left"> </a>
 
-                    <button className={estiloBotao} onClick={ (e) => deletar(e)}>Deletar
+                    <button className={estiloBotao} onClick={(e) => deletar(e)}>Deletar
                         <i className="material-icons right">clear</i>
                     </button>
                 </div>
